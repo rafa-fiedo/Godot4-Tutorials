efcfa859446b501e25fda2264c68f0ab5cf4efb6
Rafal F <fiedorowicz.rafal@gmail.com>
2025-06-11 15:06:09 +0200

    add input buffer

--- game/scenes/characters/playable/dwarf_durian/dwarf.gd
+++ game/scenes/characters/playable/dwarf_durian/dwarf.gd
@@ -11,19 +11,39 @@ func _physics_process(delta: float) -> void:
 	animate()
 	move_and_slide()
 
-func calculate_velocity(delta: float) -> void:
+func calculate_velocity(delta: float) -> void:	
 	add_gravity(delta)
-	check_jump()
+	jump_logic()
 	move_left_right()
-		
+	
 func add_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+# as a extra task you can try to optimalize this code by
+# checking the last state of 'is on floor' and start timer only once
+# instead every frame
+# i can also do more complicated solution later in the video
+# https://www.youtube.com/watch?v=RFix_Kg2Di0
+
+# coyoyte time -> something around 0.15
+func jump_logic():
+	if is_on_floor():
+		$CoyoteTimer.start()
 		
-func check_jump():
-	if Input.is_action_just_pressed("game_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		$Sound.play_jump()
+	if Input.is_action_just_pressed("game_jump"):
+		$InputBufferTimer.start()
+	
+	if not $InputBufferTimer.is_stopped():
+		if is_on_floor() or not $CoyoteTimer.is_stopped():
+			make_jump()
+			$CoyoteTimer.stop()
+			$InputBufferTimer.stop()
+
+			
+func make_jump():
+	velocity.y = JUMP_VELOCITY
+	$Sound.play_jump()
 	
 func move_left_right():
 	var direction := Input.get_axis("game_left", "game_right")
@@ -33,17 +53,23 @@ func move_left_right():
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 func animate():
-	face_good_direction()
+	if velocity.x < 0:
+		is_look_left = true
+	elif velocity.x > 0:
+		is_look_left = false
+
+	face_good_direction()	
 	if not is_on_floor():
-		pass
-		# play fly animation
-		
+		if velocity.y < 0:
+			$AnimationPlayer.play("FlyUp")
+		else:
+			$AnimationPlayer.play("FlyBottom")
+		return
+	
 	if velocity.x == 0:
 		$AnimationPlayer.play("Idle")
 		return
 	
-	is_look_left = velocity.x < 0
-
 	$AnimationPlayer.play("Running")
 
 func face_good_direction() -> void:
@@ -68,3 +94,9 @@ func _on_dead_zone_detector_area_entered(_area: Area2D) -> void:
 	
 func reset_scene():
 	get_tree().reload_current_scene()
+	
+func get_coyote_time():
+	return round(($CoyoteTimer.time_left) * 100) / 100.0
+	
+func get_input_buffer_time():
+	return round(($InputBufferTimer.time_left) * 100) / 100.0
--- game/scenes/test_scene/status_grid.gd
+++ game/scenes/test_scene/status_grid.gd
@@ -2,3 +2,5 @@ extends GridContainer
 
 func _physics_process(_delta: float) -> void:
 	$IsOnFloor.text = str(%Player.is_on_floor())
+	$CoyoteTime.text = str(%Player.get_coyote_time())
+	$InputBuffer.text = str(%Player.get_input_buffer_time())
